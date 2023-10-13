fbb3eaf3ba0799051464b24b136293be2f5577fb
Junn Eric Timoteo <juntyms@yahoo.com>
2023-10-13 17:58:25 +0400

    updated info

--- plugin.php
+++ plugin.php
@@ -2,24 +2,24 @@
 /**
  * Plugin Template
  *
- * @package     TO FILL
+ * @package     wmcrawler
  * @author      Mathieu Lamiot
- * @copyright   TO FILL
+ * @copyright   2023
  * @license     GPL-2.0-or-later
  *
  * @wordpress-plugin
- * Plugin Name: TO FILL
- * Version:     TO FILL
- * Description: TO FILL
+ * Plugin Name: WpCrawler
+ * Version:     1
+ * Description: Homepage crawler
  * Author:      Mathieu Lamiot
  */
 
 namespace ROCKET_WP_CRAWLER;
 
-define( 'ROCKET_CRWL_PLUGIN_FILENAME', __FILE__ ); // Filename of the plugin, including the file.
+define('ROCKET_CRWL_PLUGIN_FILENAME', __FILE__); // Filename of the plugin, including the file.
 
-if ( ! defined( 'ABSPATH' ) ) { // If WordPress is not loaded.
-	exit( 'WordPress not loaded. Can not load the plugin' );
+if (! defined('ABSPATH')) { // If WordPress is not loaded.
+    exit('WordPress not loaded. Can not load the plugin');
 }
 
 // Load the dependencies installed through composer.
@@ -33,10 +33,11 @@ require_once __DIR__ . '/src/support/exceptions.php';
  *
  * @return void
  */
-function wpc_crawler_plugin_init() {
-	$wpc_crawler_plugin = new Rocket_Wpc_Plugin_Class();
+function wpc_crawler_plugin_init()
+{
+    $wpc_crawler_plugin = new Rocket_Wpc_Plugin_Class();
 }
-add_action( 'plugins_loaded', __NAMESPACE__ . '\wpc_crawler_plugin_init' );
+add_action('plugins_loaded', __NAMESPACE__ . '\wpc_crawler_plugin_init');
 
-register_activation_hook( __FILE__, __NAMESPACE__ . '\Rocket_Wpc_Plugin_Class::wpc_activate' );
-register_uninstall_hook( __FILE__, __NAMESPACE__ . '\Rocket_Wpc_Plugin_Class::wpc_uninstall' );
+register_activation_hook(__FILE__, __NAMESPACE__ . '\Rocket_Wpc_Plugin_Class::wpc_activate');
+register_uninstall_hook(__FILE__, __NAMESPACE__ . '\Rocket_Wpc_Plugin_Class::wpc_uninstall');
